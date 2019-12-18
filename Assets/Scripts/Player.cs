5b72dc230e43e2540d06141700c8c113c9eda9a9
BrenoQGC <brenoqcarvalho@gmail.com>
2019-12-18 16:18:39 -0300

    Inserido script e waypoints de movimentação dos inimigos

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Player : MonoBehaviour
 {
+    // configuration parameters
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 1f;
     [SerializeField] GameObject laserPrefab=null;
@@ -13,68 +13,61 @@ public class Player : MonoBehaviour
 
     Coroutine firingCoroutine;
 
-    float xMin, xMax;
-    float yMin, yMax;
-    
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    // Use this for initialization
     void Start()
     {
         SetUpMoveBoundaries();
     }
+
     // Update is called once per frame
     void Update()
     {
         Move();
         Fire();
     }
-
     private void Fire()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            firingCoroutine = StartCoroutine(KeepFiring());
+            firingCoroutine = StartCoroutine(FireContinuously());
         }
         if (Input.GetButtonUp("Fire1"))
         {
             StopCoroutine(firingCoroutine);
         }
     }
-
-    IEnumerator KeepFiring()
+    IEnumerator FireContinuously()
     {
-        while (true)
-        { 
-        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
-        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
-
-        yield return new WaitForSeconds(projectileFiringPeriod);
+            while (true)
+            {
+                GameObject laser = Instantiate(
+                          laserPrefab,
+                    transform.position,
+                    Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
         }
+    }
+
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
     }
     private void SetUpMoveBoundaries()
     {
         Camera gameCamera = Camera.main;
-        SetXBoundarie(gameCamera);
-        SetYBoundarie(gameCamera);
-    }
-
-    private void SetYBoundarie(Camera gameCamera)
-    {
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
-    }
-
-    private void SetXBoundarie(Camera gameCamera)
-    {
         xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
         xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
-    }
-
-     private void Move()
-    {
-        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
-        var newXPos = Mathf.Clamp( transform.position.x + deltaX , xMin, xMax);
-        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
-        transform.position = new Vector2(newXPos, newYPos);
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
