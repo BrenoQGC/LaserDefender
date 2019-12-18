a70ae659fdcd8ec64f398001a4fe3e34606d3751
BrenoQGC <brenoqcarvalho@gmail.com>
2019-12-18 13:44:59 -0300

    Player movement added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float xMin, xMax;
+    float yMin, yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        SetXBoundarie(gameCamera);
+        SetYBoundarie(gameCamera);
+    }
+
+    private void SetYBoundarie(Camera gameCamera)
+    {
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    private void SetXBoundarie(Camera gameCamera)
+    {
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp( transform.position.x + deltaX , xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
